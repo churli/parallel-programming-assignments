3a0a428cf7ab60de64252bf7f3adbaa3015f3181
Tommaso Bianucci <tommaso@philae>
2018-05-12 16:29:27 +0200

    Assignment 05 completed succesfully! :)

--- A05/student/familytree_par.c
+++ A05/student/familytree_par.c
@@ -1,8 +1,62 @@
 #include "familytree.h"
+#include <omp.h>
+#include <math.h>
+
+#define T 5
+
+__attribute__((optimize(3), __target__("avx,ssse3,sse4a")))
+
+void traverseDo(tree *node, int depth);
+int compute_IQ_local(int data);
+int is_prime_local(int n);
 
 void traverse(tree *node, int numThreads){
-	
-	// put your code in here	
-
+	omp_set_num_threads(numThreads);
+	omp_set_nested(1);
+	// omp_set_max_active_levels(5);
+	#pragma omp parallel
+	#pragma omp single
+	traverseDo(node, 0);
 }
 
+void traverseDo(tree *node, int depth){
+	if(node != NULL){
+		node->IQ = compute_IQ_local(node->data);
+		genius[node->id] = node->IQ;
+		
+		#pragma omp task final(depth >= T)
+		traverse(node->right, depth+1);
+
+		#pragma omp task final(depth >= T)
+		traverse(node->left, depth+1);
+	}
+}
+
+int compute_IQ_local(int data)
+{
+
+	// nothing meaningfull, just a function that is kind of computationally expensive
+	// such that parallelising makes sense :)
+
+  int i; int sum = 0;
+	for(i = 0; i < data; i++){
+		if(is_prime_local(i))
+			sum++;
+	}
+	int modSum = sum % 100;
+	return 70 + (modSum)*(modSum)*(modSum)*(modSum)/1000000;
+}
+
+int is_prime_local(int n)
+{
+	int i, flag = 1;
+	if (n%2==0)
+		return 0;
+	for(i=3; i<=sqrt(n); i+=2){
+		if(n%i==0){
+			flag=0;
+			break;
+		}
+	}
+	return flag;
+}
