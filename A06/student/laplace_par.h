10e638908e1fe3056282c924ee674434b6c892ea
Tommaso Bianucci <tommaso@philae>
2018-05-18 20:05:08 +0200

    Assignment 6 just downloaded.

---
+++ A06/student/laplace_par.h
@@ -0,0 +1,18 @@
+#ifndef _LAPLACE_PAR_
+#define _LAPLACE_PAR_
+
+#include<omp.h>
+
+template<int SIZE>
+inline void initialize(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2])
+{
+	//TODO implement your solution in here
+}
+
+template<int SIZE>
+inline void time_step(double a[SIZE + 2][SIZE + 2], double b[SIZE + 2][SIZE + 2], int n)
+{
+	//TODO implement your solution in here
+}
+
+#endif // !_LAPLACE_PAR_
