2bbf027d55ead33a0a12b14e922852880109e672
Tommaso Bianucci <tommaso@philae>
2018-05-21 00:03:56 +0200

    Assignment 5 improved and speedup ~50 achieved.

--- A05/student/familytree_par.c
+++ A05/student/familytree_par.c
@@ -2,7 +2,7 @@
 #include <omp.h>
 #include <math.h>
 
-#define T 5
+#define T 10
 
 __attribute__((optimize(3), __target__("avx,ssse3,sse4a")))
 
@@ -15,33 +15,40 @@ void traverse(tree *node, int numThreads){
 	omp_set_nested(1);
 	// omp_set_max_active_levels(5);
 	#pragma omp parallel
-	#pragma omp single
-	traverseDo(node, 0);
+	{
+		#pragma omp single
+		{
+			traverseDo(node, 0);
+		}
+	}
 }
 
 void traverseDo(tree *node, int depth){
 	if(node != NULL){
+		#pragma omp task final(depth >= T)
+		{
+			traverse(node->right, depth+1);
+		}
+		#pragma omp task final(depth >= T)
+		{
+			traverse(node->left, depth+1);
+		}
+		
 		node->IQ = compute_IQ_local(node->data);
 		genius[node->id] = node->IQ;
-		
-		#pragma omp task final(depth >= T)
-		traverse(node->right, depth+1);
 
-		#pragma omp task final(depth >= T)
-		traverse(node->left, depth+1);
 	}
 }
 
 int compute_IQ_local(int data)
 {
-
-	// nothing meaningfull, just a function that is kind of computationally expensive
+	// nothing meaningful, just a function that is kind of computationally expensive
 	// such that parallelising makes sense :)
-
-  int i; int sum = 0;
+	int i; int sum = 0;
+	#pragma omp parallel for reduction(+:sum)
 	for(i = 0; i < data; i++){
 		if(is_prime_local(i))
-			sum++;
+			sum += 1;
 	}
 	int modSum = sum % 100;
 	return 70 + (modSum)*(modSum)*(modSum)*(modSum)/1000000;
@@ -50,10 +57,15 @@ int compute_IQ_local(int data)
 int is_prime_local(int n)
 {
 	int i, flag = 1;
+	int sqn = sqrt(n);
 	if (n%2==0)
+	{
 		return 0;
-	for(i=3; i<=sqrt(n); i+=2){
-		if(n%i==0){
+	}
+	for(i=3; i<=sqn; i+=2)
+	{
+		if(n%i==0)
+		{
 			flag=0;
 			break;
 		}
